03aea23980387156a57142d7fe8d18527d10a0b3
lloveyoutjq <2395398850@qq.com>
2020-01-20 19:17:58 +0800

    后端

--- src/main/java/com/accp/controller/PersonnelController.java
+++ src/main/java/com/accp/controller/PersonnelController.java
@@ -1,4 +1,10 @@
 package com.accp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/personnel")
 public class PersonnelController {
+
 }
