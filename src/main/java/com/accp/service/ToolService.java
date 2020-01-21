88e45799d6688b49686cd1ac2b5d57ca75a66cf9
lloveyoutjq <2395398850@qq.com>
2020-01-21 19:04:49 +0800

    后端

--- src/main/java/com/accp/service/ToolService.java
+++ src/main/java/com/accp/service/ToolService.java
@@ -6,17 +6,5 @@ import java.util.List;
 
 @Service
 public class ToolService {
-    public void ts(){
 
-    }
-
-//    private <T> void  recursionPerm  (T parentPerms, List<T> list){
-//        for(T perm : list){
-//            if(perm.getParentid() == parentPerms.getId()){
-//                Permissions newPerms = perm;
-//                parentPerms.getChildrens().add(newPerms);
-//                recursionPerm(newPerms,list);
-//            }
-//        }
-//    }
 }
