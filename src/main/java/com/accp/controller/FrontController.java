884a9716a6526293cfea766975620ff7ffce1305
chen-yuxin-zou <geerdan123@outlook.com>
2020-02-15 15:51:28 +0800

    陈宇鑫

--- src/main/java/com/accp/controller/FrontController.java
+++ src/main/java/com/accp/controller/FrontController.java
@@ -1,10 +1,35 @@
 package com.accp.controller;
 
+import com.accp.service.front.settlementService;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/front")
 public class FrontController {
+
+    @Autowired
+    settlementService SettlementService;
+
+    @RequestMapping("/bill")
+    public Map<String,Object> bill(String cashierId, String status, String carNumber, String name, String counselorName, String tname, String settlementStatus, String remark,Integer page,Integer limit){
+        Map<String,Object> map = new HashMap<>();
+        PageInfo pageInfo = SettlementService.bill(cashierId, status, carNumber, name, counselorName, tname, settlementStatus, remark, page, limit);
+        map.put("code",0);
+        map.put("data",pageInfo.getList());
+        map.put("count",pageInfo.getTotal());
+        return map;
+    }
+
+    @RequestMapping("/rollback")
+    public String rollback(String saleid){
+        SettlementService.updateByPrimaryKeySelective(saleid);
+        return "1";
+    }
 
 }
--- src/main/java/com/accp/controller/MaintenanceController.java
+++ src/main/java/com/accp/controller/MaintenanceController.java
@@ -3,6 +3,7 @@ package com.accp.controller;
 import com.accp.domain.MaintainRescue;
 import com.accp.service.completed.CompletedService;
 import com.accp.service.front.settlementService;
+import com.accp.service.repair.RepairService;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +19,10 @@ public class MaintenanceController {
     CompletedService completedService;
 
     @Autowired
-    settlementService settlementService;
+    settlementService SettlementService;
+
+    @Autowired
+    RepairService repairService;
 
     @GetMapping("/selectCompleted")
     public Map<String,Object> selectCompleted(String number,String carNumber,String frameNumber,String eligibility,Integer page,Integer limit){
@@ -34,7 +38,56 @@ public class MaintenanceController {
     public Map<String,Object> rescue(String number){
         Map<String,Object> map = new HashMap<>();
         map.put("code",0);
-        map.put("data",settlementService.rescue(number));
+        map.put("data",SettlementService.rescue(number));
         return map;
     }
+
+    @RequestMapping("/vehiclesInOperation")
+    public Map<String,Object> vehiclesInOperation(Integer page,Integer limit){
+        Map<String,Object> map = new HashMap<>();
+        PageInfo pageInfo = repairService.vehiclesInOperation(page,limit);
+        map.put("code",0);
+        map.put("code",pageInfo.getList());
+        map.put("count",pageInfo.getTotal());
+        return map;
+    }
+
+    @RequestMapping("/maintenanceHistory")
+    public Map<String,Object> maintenanceHistory(String carnumber,Integer pages,Integer limit){
+        Map<String,Object> map = new HashMap<>();
+        PageInfo pageInfo = repairService.maintenanceHistory(carnumber, pages, limit);
+        map.put("code",0);
+        map.put("code",pageInfo.getList());
+        map.put("count",pageInfo.getTotal());
+        return map;
+    }
+
+    @RequestMapping("/items")
+    public Map<String,Object> items(String carNumber,Integer pages,Integer limit){
+        Map<String,Object> map = new HashMap<>();
+        PageInfo pageInfo = repairService.items(carNumber, pages, limit);
+        map.put("code",0);
+        map.put("code",pageInfo.getList());
+        map.put("count",pageInfo.getTotal());
+        return map;
+    }
+
+    @RequestMapping("/rescueS")
+    public Map<String,Object> rescueS(String carnumber){
+        Map<String,Object> map = new HashMap<>();
+        map.put("code",0);
+        map.put("data",repairService.rescueS(carnumber));
+        return map;
+    }
+
+
+    @RequestMapping("/repairS")
+    public Map<String,Object> repairS(String carnumber){
+        Map<String,Object> map = new HashMap<>();
+        map.put("code",0);
+        map.put("data",repairService.repairS(carnumber));
+        return map;
+    }
+
+
 }
