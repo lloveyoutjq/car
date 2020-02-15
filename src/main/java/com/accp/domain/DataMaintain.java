edb07f0b0df5c7ee65f3032145db20604ded08ce
luoliangwu <1752811601@qq.com>
2020-02-15 15:36:56 +0800

    罗亮武

--- src/main/java/com/accp/domain/DataMaintain.java
+++ src/main/java/com/accp/domain/DataMaintain.java
@@ -1,9 +1,22 @@
 package com.accp.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataMaintain {
     private Integer id;
 
     private String maintainname;
+
+    public List<DataMaintain> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<DataMaintain> children) {
+        this.children = children;
+    }
+
+    private List<DataMaintain> children = new ArrayList<>();
 
     private String default1;
 
