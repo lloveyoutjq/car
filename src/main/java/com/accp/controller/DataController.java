8b28aca9aa28db5f7aa05ebc11463c61bb7a2bec
lloveyoutjq <2395398850@qq.com>
2020-02-19 14:40:51 +0800

    谭佳庆

--- src/main/java/com/accp/controller/DataController.java
+++ src/main/java/com/accp/controller/DataController.java
@@ -1,7 +1,12 @@
 package com.accp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/data")
